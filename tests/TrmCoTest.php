49a5eb00cf5de3b5dfb6d41b5bd865e9221613ac
Andres Aya <andresayac@gmail.com>
2023-07-27 18:38:18 -0500

    add tests with new methods

--- tests/TrmCoTest.php
+++ tests/TrmCoTest.php
@@ -7,33 +7,77 @@ class TrmCoTest extends TestCase
 {
     public function testQuery()
     {
-        $trmcol = new Trmco();
-        $response = $trmcol->query('2023-07-22');
-        
-        // Aquí puedes hacer afirmaciones sobre la respuesta.
-        // Por ejemplo, podrías verificar que la respuesta es un objeto y tiene ciertas propiedades.
-        $this->assertIsObject($response);
-        $this->assertObjectHasAttribute('return', $response);
+        $trmco = new Trmco();
+        $response = $trmco->query('2023-01-01')->get();
 
-        // Afirmaciones adicionales sobre la propiedad 'return'
-        $this->assertIsObject($response->return);
-        $this->assertObjectHasAttribute('value', $response->return);
-        $this->assertIsFloat($response->return->value);
+        // Verificar que la respuesta es un objeto y tiene ciertas propiedades.
+        $this->assertIsObject($response);
+        $this->assertObjectHasAttribute('value', $response);
+
+        // Afirmaciones adicionales sobre la propiedad 'value'
+        $this->assertIsFloat($response->value);
     }
+
 
     public function testQueryWithInvalidDate()
     {
         $this->expectException(InvalidArgumentException::class);
 
-        $trmcol = new Trmco();
-        $trmcol->query('not-a-date');
+        $trmco = new Trmco();
+        $trmco->query('not-a-date')->get();
     }
 
     public function testQueryWithDateBefore2013()
     {
         $this->expectException(InvalidArgumentException::class);
 
-        $trmcol = new Trmco();
-        $trmcol->query('2012-12-31');
+        $trmco = new Trmco();
+        $trmco->query('2012-12-31')->get();
+    }
+
+    public function testCopToUsd()
+    {
+        $trmco = new Trmco();
+        $trmco->query('2023-01-01');
+
+        // Aquí usamos un valor de ejemplo para COP, podrías usar un valor real.
+        $result = $trmco->copToUsd(10000);
+
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey('usd', $result);
+        $this->assertArrayHasKey('cop', $result);
+        $this->assertArrayHasKey('trm', $result);
+        $this->assertArrayHasKey('date', $result);
+    }
+
+    public function testCopToUsdWithNonNumericValue()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $trmco = new Trmco();
+        $trmco->query('2023-01-01')->copToUsd('not-a-number');
+    }
+
+    public function testUsdToCop()
+    {
+        $trmco = new Trmco();
+        $trmco->query('2023-01-01');
+
+        // Aquí usamos un valor de ejemplo para USD, podrías usar un valor real.
+        $result = $trmco->usdToCop(100);
+
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey('usd', $result);
+        $this->assertArrayHasKey('cop', $result);
+        $this->assertArrayHasKey('trm', $result);
+        $this->assertArrayHasKey('date', $result);
+    }
+
+    public function testUsdToCopWithNonNumericValue()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $trmco = new Trmco();
+        $trmco->query('2023-01-01')->usdToCop('not-a-number');
     }
 }
